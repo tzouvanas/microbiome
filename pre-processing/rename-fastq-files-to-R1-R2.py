57bc989c59c3b08838aef53ea71327b3add529fe
tzouvanas <ktzuvan.personal@gmail.com>
2020-07-09 14:24:15 +0300

    Update Q&A When renaming replace first . with - character

--- pre-processing/rename-fastq-files-to-R1-R2.py
+++ pre-processing/rename-fastq-files-to-R1-R2.py
@@ -6,8 +6,20 @@ files_of_current_directory = [f for f in os.listdir(curent_directory) if os.path
 
 for file_of_current_directory in files_of_current_directory:
     if file_of_current_directory.endswith('.fastq'):
+        
         full_path_name = os.path.join(curent_directory, file_of_current_directory)
-        if '@R' in full_path_name:
-            os.rename(full_path_name, full_path_name.replace('@R', '_R2'))
-        if '@F' in full_path_name:
-            os.rename(full_path_name, full_path_name.replace('@F', '_R1'))
+
+        newFileName = None
+        if '@F' in file_of_current_directory:
+            newFileName = file_of_current_directory.replace('@F', '_R1')
+
+        if '@R' in file_of_current_directory:
+            newFileName = file_of_current_directory.replace('@R', '_R2')
+
+        splits = newFileName.split('.')
+        if len(splits) != 3:
+            continue
+
+        newFileName = splits[0] + '-' + splits[1] + '.' + splits[2]
+
+        os.rename(full_path_name, full_path_name.replace(file_of_current_directory, newFileName))
