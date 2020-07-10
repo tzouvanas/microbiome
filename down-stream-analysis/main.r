e93f0de47aaeca575c29d7aca56bf913e45ecc51
tzouvanas <ktzuvan.personal@gmail.com>
2020-07-10 21:48:26 +0300

    Introduce OTU table as it came from pre-processing step. Cluster OTUs using k-median clustering.

---
+++ down-stream-analysis/main.r
@@ -0,0 +1,24 @@
+# cleanup console
+cat("\014")  
+
+# cleanup environment variables
+rm(list=ls(all=TRUE))
+
+source('k-medians.r')
+# read otu table
+otu_table <- read.delim2("./data/otu-table.txt", header=T, sep="\t", row.names = 1)
+otu_matrix <- as.matrix(otu_table)
+
+# extract sample, time-slots
+row_names <- row.names(otu_table)
+samples <- lapply(row_names, function(i){substr(i, start = 2, stop = 4)})
+timeSlots <- lapply(row_names, function(i){substr(i, start = 5, stop = 7)})
+
+# calculate distances between samples
+distance_matrix <- dist(t(otu_matrix), method = 'manhattan', diag = T, upper = T)
+
+centroid_allocations <- kmedians(t(otu_matrix), 3)
+plot(centroid_allocations)
+
+
+
