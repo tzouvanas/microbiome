8d6f2c1043ebed8e603a53d07e09c51fd2448fa7
tzouvanas <ktzuvan.personal@gmail.com>
2020-09-19 21:27:14 +0300

    move all otu functionality to otus.r

--- down-stream-analysis/otus.r
+++ down-stream-analysis/otus.r
@@ -1,3 +1,15 @@
+otus.all.records.for.sample <- function(X, sample_id){
+  row_names <- row.names(X)
+  indeces <- which(startsWith(row_names, paste("X", sample_id, sep = '')))
+  return(X[indeces, ])
+}
+
+otus.all.records.for.timepoint <- function(otu_table, timepoint){
+  row_names <- row.names(otu_table)
+  indeces <- which(endsWith(row_names, timepoint))
+  return(otu_table[indeces, ])
+}
+
 otus.normalize <- function(otus, otu_threashold){
   
   # calculate otu percentages per sample
